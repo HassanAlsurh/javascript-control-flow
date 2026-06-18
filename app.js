a15fcba466049b96e4a9192c55b31127a0c60330
HassanAlsurh <HassanAlsurh@gmail.com>
2026-06-18 11:28:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const age = 12
+const hasID = true
+const withParent = true
+
+
+if (age >= 16 && hasID) {
+ console.log("Can Enter")
+}
+else if (withParent){
+    console.log("Can Enetr with Parent")
+}
+else {
+    console.log("Cannot Enter")
+}
